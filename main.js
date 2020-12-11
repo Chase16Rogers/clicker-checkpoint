0bb3388e0bf105cd4ecec1162f0b78cbb73125e0
Chase16Rogers <chase16rogers@gmail.com>
2020-12-11 14:41:36 -0700

    Created shop object & timeout

--- main.js
+++ main.js
@@ -1,5 +1,34 @@
+
 let gold = 0
 let multiplyer = 1
+let auto = 8
+
+let shop = {
+  miner: {
+    cost: 50,
+    add: 1,
+  },
+  pirate:{
+    cost: 100,
+    add: 5,
+  },
+  entrepreneur: {
+  cost: 200,
+  add: 10,
+  },
+  tycoon: {
+    cost: 500,
+    add: 20,
+  },
+  ceo: {
+    cost: 1000,
+    add: 50,
+  },
+  goldmine: {
+    cost: 5000,
+    add: 100,
+  }
+}
 
 function addGold() {
   gold += 1 * multiplyer
@@ -10,8 +39,33 @@ function drawGold() {
   document.getElementById("gold").innerText = gold
 }
 
+function market(item) {
+  switch(item){
+  case "miner":
+    shop.miner.cost += 15
+    auto += 1
+  break;
+  case "pirate":
+    shop.pirate.cost += 25
+    auto += 5
+  break;
+  case "entrepreneur":
+    shop.entrepreneur.cost += 50
+    auto += 10
+  break;
+  case "tycoon":
+    shop.tycoon.cost += 250
+    auto += 20
+  }
+}
+ 
 
+function goldPerSecond() {
+  gold += auto
+  console.log(gold)
+  drawGold()
+}
 
-
+setInterval(goldPerSecond, 1000)
 
 drawGold()
