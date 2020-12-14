1f14b3339a79dc469258c460fa9aa1b5265aaeab
Chase16Rogers <chase16rogers@gmail.com>
2020-12-13 20:35:39 -0700

    Technically Finished?

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ let gold = 0
 let multiplyer = 1
 let auto = 0
 
+
 let shop = {
   pickaxe: {
     name: "Pickaxe",
@@ -56,35 +57,40 @@ let shop = {
 }
 
 function addGold() {
-  gold += 1 * multiplyer
-  drawGold()
-  drawShop()
+  setTimeout(() => {
+    gold += 1 * multiplyer
+    drawGold()
+    displayPurchase()
+  }, 5)
 }
 
 function drawGold() {
   document.getElementById("gold").innerText = gold
+  displayPurchase()
 }
 let shopKey = Object.keys(shop)
+
 
 function drawShop() {
   let template = ""
   for (let i = 0; i < shopKey.length; i++) {
-    let hide = ""
     let goods = shopKey[i]
-
-    if (shop[goods].cost >= gold) { hide = "d-none" }
-
+    let goodName = shop[goods].name
+    let goodButt = goods
+    let goodButtId = goods + "butt"
     template += `<div class=" col-md-6 d-flex justify-content-center align-items-center" >
     <img src="${shop[goods].img}" alt="${shop[goods]}" class="shop-img">
-        <div>
-        <h3>${shop[goods].name} x<span>3</span></h3>
-        <h5>+ ${shop[goods].add} gold per second</h5>
-        <h5 id="${goods}">Cost: ${shop[goods].cost}</h5>
-        <button class="btn btn-success ${hide}" onclick="market('${goods}')">Purchase</button>
+    <div>
+        <h3>${shop[goods].name} x<span id="${goodName + 'count'}">${shop[goods].count}</span></h3>
+        <h5 id="diff">+ ${shop[goods].add} gold per second</h5>
+        <h5 >Cost: <span id="${goodName + "cost"}">${shop[goods].cost}</span> gold</h5>
+        <button id="${goodButtId}" class="btn bg-maroon text-gold d-none" onclick="market('${goodButt}')">Purchase</button>
         </div>
-      </div>`
+        </div>`
+    if (gold >= shop[goods].cost) { }
   }
   document.getElementById("shop").innerHTML = template
+
 }
 
 function market(item) {
@@ -93,53 +99,52 @@ function market(item) {
       gold -= shop.pickaxe.cost
       multiplyer *= 2
       shop.pickaxe.cost *= 3
-    case "miner":
-      gold -= shop.miner.cost
-      shop[item].cost += 15
-      shop.miner.count++
-      auto += 1
+    default:
+      if (gold >= shop[item].cost) {
+        gold -= shop[item].cost
+        shop[item].cost += Math.floor(shop[item].cost / 4)
+        shop[item].count++
+        auto += shop[item].add
+      }
       break;
-    case "pirate":
-      gold -= shop.pirate.cost
-      shop.pirate.cost += 25
-      shop.pirate.count++
-      auto += 5
-      break;
-    case "entrepreneur":
-      gold -= shop.entrepreneur.cost
-      shop.entrepreneur.cost += 50
-      shop.entrepreneur.count++
-      auto += 10
-      break;
-    case "tycoon":
-      gold -= shop.tycoon.cost
-      shop.tycoon.cost += 250
-      shop.tycoon.count++
-      auto += 20
   }
-
+  if (shop[item].cost >= gold) {
+    document.getElementById(item + 'butt').classList.add("d-none")
+  }
+  document.getElementById(shop[item].name + 'cost').innerText = shop[item].cost
+  document.getElementById(shop[item].name + 'count').innerText = shop[item].count
+  document.getElementById("goldps").innerText = auto
+  document.getElementById("goldpc").innerText = multiplyer
+  if (item == "pickaxe") { document.getElementById("diff").innerText = "x 2 gold per click" }
+  drawGold()
+  displayPurchase()
 }
+
 
 function displayPurchase() {
+  let hider = "d-none"
   for (let i = 0; i < shopKey.length; i++) {
-    let goods = shop[shopKey[i]]
-    if (goods.cost >= gold) {
-      return 0
-      // document.getElementbyID("miner").classlist.add("d-none")
+    let good = shopKey[i]
+    let upgradeName = shop[good].name
+    let upgradeCost = shop[good].cost
+    let upgradeCount = shop[good].count
+    if (upgradeCost <= gold) {
+      document.getElementById(good + "butt").classList.remove("d-none")
+    } else {
+      document.getElementById(good + "butt").classList.add("d-none")
     }
-    drawShop()
   }
+
 }
+
 
 
 function goldPerSecond() {
   gold += auto
   drawGold()
-
 }
 
 setInterval(goldPerSecond, 1000)
-
 drawShop()
 displayPurchase()
 drawGold()
